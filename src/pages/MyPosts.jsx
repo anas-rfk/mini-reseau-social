7ae4a01d9c1b472deb780ccfa4ecf3586e9cd28e
anas-rfk <anasrafik894@gmail.com>
2026-01-26 18:37:23 +0100

    fix: update posts list after delete in my-posts

--- src/pages/MyPosts.jsx
+++ src/pages/MyPosts.jsx
@@ -1,12 +1,50 @@
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../selectors/auth.selectors";
+import { fetchPosts } from "../features/posts/thunks";
+import { fetchUsers } from "../features/users/thunks";
+import PostCard from "../components/PostCard";
 
 function MyPosts() {
-  const { pathname } = useLocation();
+  const currentUser = useSelector(selectCurrentUser);
+
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchPosts().then(setPosts);
+    fetchUsers().then(setUsers);
+  }, []);
+
+  const getAuthorName = (authorId) => {
+    const user = users.find((u) => String(u.id) === String(authorId));
+    return user ? user.username : "Unknown";
+  };
+
+  const handleDeleted = (deletedId) => {
+    setPosts((prev) => prev.filter((p) => String(p.id) !== String(deletedId)));
+  };
+
+  const myPosts = posts.filter(
+    (post) => String(post.authorId) === String(currentUser?.id)
+  );
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>My Posts</h1>
-      <p>pathname: {pathname}</p>
+      <h1>Mes posts</h1>
+
+      {myPosts.length === 0 ? (
+        <p>Aucun post</p>
+      ) : (
+        myPosts.map((post) => (
+          <PostCard
+            key={post.id}
+            post={post}
+            authorName={getAuthorName(post.authorId)}
+            onDeleted={handleDeleted}
+          />
+        ))
+      )}
     </div>
   );
 }
